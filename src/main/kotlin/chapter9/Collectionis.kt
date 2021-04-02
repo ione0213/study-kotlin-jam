130e1e6cfa545984fada5c1ebdbd5c8a2d4718a5
Yu Chen <ione0213@gmail.com>
2021-04-02 22:07:35 +0800

    Chapter 9 example map

--- src/main/kotlin/chapter9/Collectionis.kt
+++ src/main/kotlin/chapter9/Collectionis.kt
@@ -1,7 +1,10 @@
 package chapter9
 
+data class Recipe(var name: String)
+
 fun main() {
-    val mShoppingList = mutableListOf("Tea", "Eggs", "Milk")
+//    val mShoppingList = mutableListOf("Tea", "Eggs", "Milk")
+    var mShoppingList = mutableListOf("Tea", "Eggs", "Milk")
     println("mShoppingList original : $mShoppingList")
     val extraShopping = listOf("Cookie", "Sugar", "Eggs")
     mShoppingList.addAll(extraShopping)
@@ -13,4 +16,50 @@ fun main() {
     println("mShoppingList sorted : $mShoppingList")
     mShoppingList.reverse()
     println("mShoppingList reversed : $mShoppingList")
+
+    val mShoppingSet = mShoppingList.toMutableSet()
+    println("mShoppingSet : $mShoppingSet")
+    val moreShopping = setOf("Chives", "Spinach", "Milk")
+    mShoppingSet.addAll(moreShopping)
+    println("mShoppingSet items added : $mShoppingSet")
+    mShoppingList = mShoppingSet.toMutableList()
+    println("mShoppingList new version : $mShoppingList")
+
+    val list = listOf(Duck(), Duck(17))
+    val set = setOf(Duck(), Duck(17))
+    println(set)
+
+    var pets: MutableList<String> = mutableListOf()
+
+    val r1 = Recipe("Chicken Soup")
+    val r2 = Recipe("Quinoa Salad")
+    val r3 = Recipe("Thai Curry")
+    val r4 = Recipe("Jambalaya")
+    val r5 = Recipe("Sausage Rolls")
+    val mRecipeMap = mutableMapOf("Recipe1" to r1, "Recipe2" to r2, "Recipe3" to r3)
+    println("mRecipeMap original: $mRecipeMap")
+    val recipesToAdd = mapOf("Recipe4" to r4, "Recipe5" to r5)
+    mRecipeMap.putAll(recipesToAdd)
+    println("mRecipeMap updated: $mRecipeMap")
+    if(mRecipeMap.containsKey("Recipe1")) {
+        println("Recipe1 is ${mRecipeMap.getValue("Recipe1")}")
+        println(mRecipeMap.entries)
+    }
+
 }
+
+class Duck(val size: Int = 17) {
+//    override fun equals(other: Any?): Boolean {
+//        return true
+//    }
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other is Duck && size == other.size) return true
+        return false
+    }
+
+    override fun hashCode(): Int {
+        return 7
+    }
+}
+
